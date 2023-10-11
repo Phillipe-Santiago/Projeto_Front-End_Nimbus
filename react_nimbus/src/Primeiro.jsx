5adfef4ea2386c5c1d617d1c9a1a316f65482c0b
PHILLIPE SANTIAGO ROSSO DI SAN SECONDO <202051942185@alunos.ibmec.edu.br>
2023-10-11 10:19:15 -0300

    commit react branch Phillipe

---
+++ react_nimbus/src/Primeiro.jsx
@@ -0,0 +1,9 @@
+const Primeiro = () => {
+    return (<div>
+                <h5>Mini título</h5>
+                <p>Meu texto</p>
+            </div>
+            );
+};
+
+export default Primeiro;
