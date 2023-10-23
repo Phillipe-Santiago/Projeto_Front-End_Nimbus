259c9a7763890d91e03cec796f649d3242bf5ae6
PHILLIPE SANTIAGO ROSSO DI SAN SECONDO <202051942185@alunos.ibmec.edu.br>
2023-10-23 10:27:51 -0300

    commit inicial

---
+++ React/react_nimbus/src/pages/Segundo.jsx
@@ -0,0 +1,12 @@
+import LinkPrimeiro from "../components/LinkPrimeiro";
+
+const Segundo = () => {
+    return (<div>
+                <h5>Segundo</h5>
+                <LinkPrimeiro />
+                <a href="/" class="button">Botão 4</a>
+        </div>
+            );
+};
+
+export default Segundo;
