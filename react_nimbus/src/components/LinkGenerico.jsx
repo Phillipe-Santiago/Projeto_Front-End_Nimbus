f41d14ed8417c84e24e9210a0c30eb2d2d2d668f
PHILLIPE SANTIAGO ROSSO DI SAN SECONDO <202051942185@alunos.ibmec.edu.br>
2023-11-06 08:47:09 -0300

    commit

---
+++ react_nimbus/src/components/LinkGenerico.jsx
@@ -0,0 +1,4 @@
+const LinkGenerico = (props) => (
+    <a href={props.endereco}>{props.texto}<a/>
+);
+export default LinkGenerico;
---
+++ react_nimbus/src/pages/Primeiro.jsx
@@ -0,0 +1,13 @@
+import LinkSegundo from "../components/LinkSegundo";
+
+const Primeiro = () => {
+    return (<div>
+                <h5>Página inicial</h5>
+                <p>Dashboard</p>
+                <LinkSegundo /><br/>
+                <LinkGenerico>
+            </div>
+            );
+};
+
+export default Primeiro;
