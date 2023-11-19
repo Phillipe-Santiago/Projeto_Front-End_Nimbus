73d933d8fec3bce516cdaf7cc89b828909df3240
Phillipe Secondo <philliperosso29@gmail.com>
2023-11-19 16:15:02 -0300

    adicionado logo nimbus

--- projeto_cronos/src/components/header/Header.jsx
+++ projeto_cronos/src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import "./header.css";
 const Header = () => {
     return (
         <div id='header'>
-        <h1>CRONOS city</h1>
+        <h1><img src="https://nimbusmeteorologia.com.br/assets/logo.ae8a68ef.svg"></img></h1>
         <button onclick="window.location.href=https://www.youtube.com">Sobre Nós</button>
     </div>
     )
