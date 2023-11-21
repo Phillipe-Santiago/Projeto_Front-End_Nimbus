0f16b08b6f9db501e2246437da9eff022978cf16
PHILLIPE SANTIAGO ROSSO DI SAN SECONDO <202051942185@alunos.ibmec.edu.br>
2023-11-21 17:12:25 -0300

    router funcionando

--- projeto_cronos/src/index.js
+++ projeto_cronos/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import SobreNos from './pages/SobreNos';
 import MainPage from './pages/MainPage';
+
+const router = createBrowserRouter([
+  {
+    path: "sobrenos",
+    element: <SobreNos/>
+  },
+  {
+    path: "principal",
+    element: <MainPage/>
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MainPage/>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
