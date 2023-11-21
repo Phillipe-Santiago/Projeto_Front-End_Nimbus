67bacabc1ac36afff94d9168fea83cf6c67aeb56
PHILLIPE SANTIAGO ROSSO DI SAN SECONDO <202051942185@alunos.ibmec.edu.br>
2023-11-21 19:27:26 -0300

    tentativa de realizar o filtro

--- projeto_cronos/src/components/header/Header.jsx
+++ projeto_cronos/src/components/header/Header.jsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
+import React, { useState } from 'react';
 import "./header.css";
+import Modal from "../modal/Modal";
 
 
 const Header = () => {
+
+
     return (
+
         <div id='header'>
-            <h1><img src="https://nimbusmeteorologia.com.br/assets/logo.ae8a68ef.svg"></img></h1>
+
+
+            <h1><img src="https://nimbusmeteorologia.com.br/assets/logo.ae8a68ef.svg"></img>
+
+            </h1>
             <div>
-                <button id="button_filter">Filtrar</button>
+
             </div>
             <Link to="sobrenos">
                 <button id='button_sn'>Sobre Nós</button>
--- projeto_cronos/src/pages/MainPage.jsx
+++ projeto_cronos/src/pages/MainPage.jsx
@@ -4,8 +4,19 @@ import Footer from "../components/footer/Footer";
 import Mapa from "../components/mapa/Mapa";
 import Grafico from "../components/grafico/Grafico";
 import "./mainPage.css";
+import Modal from "../components/modal/Modal";
 
 const MainPage = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   const [selectedDataset, setSelectedDataset] = useState("Copacabana");
 
   const handleMarkerClick = (dataset) => {
@@ -14,17 +25,27 @@ const MainPage = () => {
 
   return (
     <div id='mainPage'>
+      <div>
+        <button onClick={openModal}>Filtrar</button>
+
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+          <h2>Conteúdo da Janela Modal</h2>
+          <p>Este é um exemplo de conteúdo dentro da janela modal.</p>
+        </Modal>
+      </div>
       <div id='conteudo'>
         <div>
-          <Mapa onMarkerClick={handleMarkerClick}/>
+          <Mapa onMarkerClick={handleMarkerClick} />
         </div>
         <div>
-          <Grafico selectedDataset={selectedDataset}/>
+          <Grafico selectedDataset={selectedDataset} />
         </div>
       </div>
       <Header />
+      
       <Footer />
     </div>
+    
   );
 };
 
