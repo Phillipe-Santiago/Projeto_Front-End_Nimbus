9a20605587093a48ef3a6c1aa32a8421a1c4caeb
“Ian <pk20lian@gmail.com>
2023-11-22 00:06:50 -0300

    pin colorido

--- projeto_cronos/src/components/mapa/Mapa.jsx
+++ projeto_cronos/src/components/mapa/Mapa.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+/* import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './mapa.css';
@@ -48,6 +48,78 @@ function Mapa({ onMarkerClick }) {
         {markers.map((marker) => (
           <Marker key={marker.info} position={marker.geocode} icon={customIcon} eventHandlers={{ click: () => handleMarkerClick(marker.info) }}>
             <Popup>{marker.info}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
+}
+
+export default Mapa; */
+
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import './mapa.css';
+import { Icon } from 'leaflet';
+
+const center = [-22.994841333340094, -43.32459988596175];
+
+const markers = [
+  {
+    geocode: [-22.967840087200422, -43.18297403633863],
+    info: "Copacabana",
+  },
+  {
+    geocode: [-23.01221940541354, -43.3079565626253],
+    info: "Jardim Oceânico",
+  },
+  {
+    geocode: [-23.01393952265379, -43.45112774159444],
+    info: "Recreio dos Bandeirantes",
+  },
+  {
+    geocode: [-22.92912195894094, -43.2507853920363],
+    info: "Tijuca",
+  },
+];
+
+const defaultIcon = new Icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/252/252025.png',
+  iconSize: [38, 38],
+});
+
+const selectedIcon = new Icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/10542/10542406.png',
+  iconSize: [38, 38],
+});
+
+function Mapa({ onMarkerClick }) {
+  const [selectedMarkerIndex, setSelectedMarkerIndex] = useState(null);
+
+  const handleMarkerClick = (marker, index) => {
+    setSelectedMarkerIndex(index);
+    if (onMarkerClick) {
+      onMarkerClick(marker.info);
+    }
+  };
+
+  return (
+    <div id='mapa'>
+      <MapContainer center={center} zoom={10} style={{ width: '100%', height: '45vh' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.de/{z}/{x}/{y}.png"
+        />
+
+        {markers.map((marker, index) => (
+          <Marker
+            key={marker.info}
+            position={marker.geocode}
+            icon={selectedMarkerIndex === index ? selectedIcon : defaultIcon}
+            eventHandlers={{ click: () => handleMarkerClick(marker, index) }}
+          >
+
           </Marker>
         ))}
       </MapContainer>
